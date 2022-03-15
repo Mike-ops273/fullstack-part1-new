06e287331730e1e815c51858dcf4e3a1032d8378
Mike-ops273 <mivaxd55@yahoo.com>
2022-03-15 19:20:17 +0200

    123

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -1,3 +1,38 @@
+const Header = (props) => { 
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+} 
+
+const Content = () => {
+  const part1 = 'Fundamentals of React '
+  const exercises1 = 10
+  const part2 = 'Using props to pass data '
+  const exercises2 = 7
+  const part3 = 'State of a component '
+  const exercises3 = 14 
+
+  return (
+    <div>
+      <p>{part1 + exercises1}</p>
+      <p>{part2 + exercises2}</p>
+      <p>{part3 + exercises3}</p>
+    </div>
+  )
+} 
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+      Total {props.exercises}
+      </p>
+    </div>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -5,23 +40,25 @@ const App = () => {
   const part2 = 'Using props to pass data'
   const exercises2 = 7
   const part3 = 'State of a component'
-  const exercises3 = 14
+  const exercises3 = 14 
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+     <Header course={course}/> 
+     <Content /> 
+     <Total exercises={exercises1 + exercises2 + exercises3}/>
     </div>
   )
 }
 
-export default App
+export default App 
+
+/**
+const course = 'Half Stack application development'
+const part1 = 'Fundamentals of React'
+const exercises1 = 10
+const part2 = 'Using props to pass data'
+const exercises2 = 7
+const part3 = 'State of a component'
+const exercises3 = 14 
+*/
