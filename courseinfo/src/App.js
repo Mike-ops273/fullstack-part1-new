5bd13fc9390c54e238256247a3e720a514e49345
Mike-ops273 <mivaxd55@yahoo.com>
2022-03-16 09:19:18 +0200

    3 calls to content component solution

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -6,7 +6,21 @@ const Header = (props) => {
   );
 };
 
-const Content = () => {
+const Content = (props) => {
+  return <div>{props.contents}</div>;
+};
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Total {props.total}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  //kaikki data
+  const course = "Half Stack application development";
   const part1 = "Fundamentals of React ";
   const exercises1 = 10;
   const part2 = "Using props to pass data ";
@@ -16,47 +30,13 @@ const Content = () => {
 
   return (
     <div>
-      <Part />
-      <Part />
-      <Part />
-    </div>
-  );
-};
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>Total {props.exercises}</p>
-    </div>
-  );
-};
-
-const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
-
-  return (
-    <div>
       <Header course={course} />
-      <Content />
-      <Total exercises={exercises1 + exercises2 + exercises3} />
+      <Content contents={part1 + exercises1} />
+      <Content contents={part2 + exercises2} />
+      <Content contents={part3 + exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   );
 };
 
 export default App;
-
-/**
-const course = 'Half Stack application development'
-const part1 = 'Fundamentals of React'
-const exercises1 = 10
-const part2 = 'Using props to pass data'
-const exercises2 = 7
-const part3 = 'State of a component'
-const exercises3 = 14
-*/
