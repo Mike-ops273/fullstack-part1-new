16e310dbc1ef871039afe62f47faaeca36e0e02d
Mike-ops273 <mivaxd55@yahoo.com>
2022-03-16 08:15:44 +0200

    code formatter

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -1,57 +1,55 @@
-const Header = (props) => { 
+const Header = (props) => {
   return (
     <div>
       <h1>{props.course}</h1>
     </div>
-  )
-} 
+  );
+};
 
 const Content = () => {
-  const part1 = 'Fundamentals of React '
-  const exercises1 = 10
-  const part2 = 'Using props to pass data '
-  const exercises2 = 7
-  const part3 = 'State of a component '
-  const exercises3 = 14 
+  const part1 = "Fundamentals of React ";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data ";
+  const exercises2 = 7;
+  const part3 = "State of a component ";
+  const exercises3 = 14;
 
   return (
     <div>
-      <p>{part1 + exercises1}</p>
-      <p>{part2 + exercises2}</p>
-      <p>{part3 + exercises3}</p>
+      <Part />
+      <Part />
+      <Part />
     </div>
-  )
-} 
+  );
+};
 
 const Total = (props) => {
   return (
     <div>
-      <p>
-      Total {props.exercises}
-      </p>
+      <p>Total {props.exercises}</p>
     </div>
-  )
-}
+  );
+};
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14 
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
 
   return (
     <div>
-     <Header course={course}/> 
-     <Content /> 
-     <Total exercises={exercises1 + exercises2 + exercises3}/>
+      <Header course={course} />
+      <Content />
+      <Total exercises={exercises1 + exercises2 + exercises3} />
     </div>
-  )
-}
+  );
+};
 
-export default App 
+export default App;
 
 /**
 const course = 'Half Stack application development'
@@ -60,5 +58,5 @@ const exercises1 = 10
 const part2 = 'Using props to pass data'
 const exercises2 = 7
 const part3 = 'State of a component'
-const exercises3 = 14 
+const exercises3 = 14
 */
