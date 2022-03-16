8d51807adbc090f7f16d638dbf0699f2db0d96de
Mike-ops273 <mivaxd55@yahoo.com>
2022-03-16 20:57:04 +0200

    best solution so far

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -1,26 +1,6 @@
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  );
-};
-
-const Content = (props) => {
-  return <div>{props.contents}</div>;
-};
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>Total {props.total}</p>
-    </div>
-  );
-};
-
 const App = () => {
-  //kaikki data
-  const course = "Half Stack application development";
+  //all data
+  const course = "Half Stack application development ";
   const part1 = "Fundamentals of React ";
   const exercises1 = 10;
   const part2 = "Using props to pass data ";
@@ -31,10 +11,40 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content contents={part1 + exercises1} />
-      <Content contents={part2 + exercises2} />
-      <Content contents={part3 + exercises3} />
+      <Content content={part1 + exercises1} />
+      <Content content={part2 + exercises2} />
+      <Content content={part3 + exercises3} />
       <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  );
+};
+
+//my components
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+};
+const Content = (props) => {
+  return (
+    <div>
+      <Part stuff={props.content} />
+    </div>
+  );
+};
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.stuff}</p>
+    </div>
+  );
+};
+const Total = (props) => {
+  return (
+    <div>
+      <p>{props.total}</p>
     </div>
   );
 };
