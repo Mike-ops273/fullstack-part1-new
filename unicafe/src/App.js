3d5ce5511ff18c00f15b7668c38e281ebb3cb4c7
Mike-ops273 <mivaxd55@yahoo.com>
2022-03-22 12:38:53 +0200

    1.6 step 1 unicafe done

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const increaseGood = (good) => {
+    console.log("the good", good);
+    setGood(good);
+  };
+  const increaseNeutral = (neutral) => {
+    console.log("the neutral", neutral);
+    setNeutral(neutral);
+  };
+  const increaseBad = (bad) => {
+    console.log("the bad", bad);
+    // setBad(bad + 1); this does not work, do addition in return
+    setBad(bad);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Give feedback</h1>
+      <Button handleClick={() => increaseGood(good + 1)} text="good" />
+      <Button handleClick={() => increaseNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => increaseBad(bad + 1)} text="bad" />
+      <h2>Statistics</h2>
+      <Display display={good} text="good" />
+      <Display display={neutral} text="neutral" />
+      <Display display={bad} text="bad" />
     </div>
   );
-}
+};
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
+
+const Display = (props) => (
+  <div>
+    {props.text} {props.display}
+  </div>
+);
 
 export default App;
