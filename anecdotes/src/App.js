0c4f2dc51817b129351a818caa8d42ab4cd451c3
Mike-ops273 <mivaxd55@yahoo.com>
2022-03-30 20:29:50 +0300

    done 1.14

--- anecdotes/src/App.js
+++ anecdotes/src/App.js
@@ -10,33 +10,58 @@ const App = () => {
     "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
     "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients",
   ];
-  const [selected, setSelected] = useState(0);
 
-  const pointsArray = new Array(anecdotes.length).fill(0);
-  //const testArray = [1, 2, 3, 4, 5, 6, 7];
-  const [votes, setVote] = useState(pointsArray);
+  //initialize random state index
+  let random = Math.floor(Math.random() * anecdotes.length);
 
+  //index selector for anecdotes
+  const [selected, setSelected] = useState(random);
+  //state array filled with zeros to hold votes
+  const [votes, setVote] = useState(new Array(anecdotes.length).fill(0));
+  //track the highest vote's index in array
+  const [best, setBest] = useState(random);
+  //array for testing: const testArray = [1, 2, 3, 4, 5, 6, 7];
+  //highest vote count
+  const [highestVote, setHighestVote] = useState(0);
+
+  //generate a random index indicator for anecdotes array
   function generateRandomNumber() {
     const randomNumber = Math.floor(Math.random() * anecdotes.length);
     setSelected(randomNumber);
   }
 
+  let indexBest = 0;
   const generateVote = () => {
-    //setVote(votes[selected] + 1);
-    //console.log(votes[selected]);
+    //spread votes into copyVotes
     const copyVotes = [...votes];
-    copyVotes[selected] = copyVotes[selected] + 1;
-    console.log(copyVotes);
+    //increment selected vote
+    copyVotes[selected] += 1;
+    //store highest vote count in vote array:
+    const max = Math.max(...copyVotes);
+    //index of highest vote count in vote array
+    indexBest = copyVotes.indexOf(max);
+    setHighestVote(max);
     setVote(copyVotes);
+    setBest(indexBest);
+    console.log("index with largest vote:", indexBest);
+    console.log("highest vote count:", max);
+    console.log("array of votes:", copyVotes);
+    console.log("");
   };
 
   return (
     <div>
+      <h1>Anecdotes of the day</h1>
       {anecdotes[selected]}
       <br />
       <p>has {votes[selected]} votes</p>
       <ButtonAnecdote handleClick={() => generateRandomNumber()} />
       <ButtonVote handleClick={() => generateVote()} />
+      <h2>Anecdote with most votes</h2>
+      <br />
+      {anecdotes[best]}
+      <br />
+      has {("placeholder", highestVote)} votes
     </div>
   );
 };
